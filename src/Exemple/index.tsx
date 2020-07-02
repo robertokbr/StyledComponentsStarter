691fcf9209e124e7b70fa5002db22b632eb6388b
Roberto Junior <robertojr-13-@hotmail.com>
2020-07-01 22:23:49 -0300

    Styled Application

---
+++ src/Exemple/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Title, Container, Box } from './styles';
+
+const Exemplo: React.FC = () => (
+  <Container>
+
+    <Box>
+      <Title> Styled Components é show! </Title>
+
+    </Box>
+  </Container>
+);
+
+export default Exemplo;
---
+++ src/styles/global.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+*{
+  margin: 0;
+  padding:0;
+  outline:0;
+  box-sizing: border-box;
+}
+
+body{
+
+  --webkit-font-smoothing: antialiased;
+  max-height: 100vh;
+display:flex;
+align-items:center;
+justify-content:center;
+margin: 120px auto;
+padding: 30px 30px;
+  background: linear-gradient(-45deg, #EE7752, #E73C73, #23A6D5,#23D5AB );
+background-size: 400% 400%;
+position: relative;
+animation: change 10s ease-in-out infinite;
+}
+@keyframes change{
+0% {
+background-position: 0 50%;
+}
+50% {
+  background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0 50%;
+    }
+
+}
+
+
+body, input, button {
+  font: 16px Roboto, sans-serif;
+}
+
+
+button{
+  cursor: pointer;
+}
+
+
+
+`;
