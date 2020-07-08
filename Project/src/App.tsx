2af208677a338c0c2eed7929b599900b42b04f70
Roberto Junior <robertojr-13-@hotmail.com>
2020-07-08 17:33:27 -0300

    Re-upload project 1

---
+++ Project/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Exemplo from './Pages';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <Exemplo />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
---
+++ Project/src/styles/global.ts
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+*{
+  margin: 0;
+  padding:0;
+  outline:0;
+  box-sizing: border-box;
+}
+
+body{
+
+  --webkit-font-smoothing: antialiased;
+  background: linear-gradient(-45deg, #EE7752, #E73C73, #23A6D5,#23D5AB );
+background-size: 400% 400%;
+position: relative;
+animation: change 10s ease-in-out infinite;
+}
+@keyframes change{
+0% {
+background-position: 0 50%;
+}
+50% {
+  background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0 50%;
+    }
+
+}
+
+
+body, input, button {
+  font: 16px Roboto, sans-serif;
+}
+
+
+button{
+  cursor: pointer;
+}
+
+#root{
+height: 100vh;
+width:100vw;
+display:flex;
+align-items:center;
+justify-content:center;
+padding: 30px 30px;
+
+}
+
+`;
